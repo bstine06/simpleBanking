0cb241e630033ef244a7e2f549cd123b82727ff6
Brett Stine <r.brettstine@gmail.com>
2024-09-02 16:16:16 -0400

    Add Account class, add basic functionality

---
+++ Account.java
@@ -0,0 +1,35 @@
+public class Account {
+
+  private String accountHolderName;
+  private int accountNumber;
+  private double balance;
+
+  public Account(String accountHolderName, int accountNumber) {
+    this.accountHolderName = accountHolderName;
+    this.accountNumber = accountNumber;
+    this.balance = 0.0;
+  }
+
+  public double deposit(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Deposit amount must be greater than 0.");
+    }
+    balance += amount;
+    return balance;
+  }
+
+  public double withdrawal(double amount) {
+    if (balance - amount < 0) {
+      throw new IllegalArgumentException("Withdrawal amount greater than available balance.");
+    }
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Withdrawal amount must be greater than 0.");
+    }
+    balance -= amount;
+    return balance;
+  }
+
+  public double checkBalance() {
+    return balance;
+  }
+}
--- SimpleBanking.java
+++ SimpleBanking.java
@@ -4,17 +4,52 @@ public class SimpleBanking {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    Account account = new Account("SampleAccountHolderName", 123456);
 
-    System.out.println("Welcome to Simple Banking!");
-    System.out.println("Please select from the following options:");
+    System.out.println("\nWelcome to Simple Banking!");
 
-    System.out.println("1. Create Account");
-    System.out.println("2. Check Balance");
+    while (true) {
+      System.out.println("Please select from the following options:\n");
 
-    int choice = scanner.nextInt();
-    scanner.nextLine();
+      System.out.println("1. Check Balance");
+      System.out.println("2. Deposit");
+      System.out.println("3. Withdrawal");
+      System.out.println("4. Exit\n");
 
-    System.out.println("Your choice was:");
-    System.out.println(choice);
+      int choice = scanner.nextInt();
+      scanner.nextLine();
+      System.out.println("\n");
+
+      double currentBalance = account.checkBalance();
+      double amount = 0;
+
+      switch (choice) {
+        case 1:
+          System.out.println("Check Balance");
+          currentBalance = account.checkBalance();
+          System.out.println("Current balance: $" + String.format("%.2f", currentBalance));
+          break;
+        case 2:
+          System.out.println("Deposit");
+          amount = scanner.nextDouble();
+          currentBalance = account.deposit(amount);
+          System.out.println("Successfully deposited $" + String.format("%.2f", amount) + " to your account.");
+          System.out.println("Current balance: $" + String.format("%.2f", currentBalance));
+          break;
+        case 3:
+          System.out.println("Withdrawal");
+          amount = scanner.nextDouble();
+          currentBalance = account.withdrawal(amount);
+          System.out.println("Successfully withdrew $" + String.format("%.2f", amount) + " from your account.");
+          System.out.println("Current balance: $" + String.format("%.2f", currentBalance));
+          break;
+        case 4:
+          System.out.println("Exiting...");
+          break;
+        default:
+          System.out.println("Please enter a valid selection.");
+      }
+    }
+
   }
 }
